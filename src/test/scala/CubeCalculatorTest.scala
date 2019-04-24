7f96d7a0612dcaaf04cbae4c076ffa186d12f23a
Abhishek Mishra <mishraabhishek.mishra021@gmail.com>
2019-04-25 02:53:36 +0530

    test commit

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -1,6 +1,6 @@
 class CubeCalculatorTest extends org.scalatest.FunSuite {
   test("CubeCalculator.cube") {
     assert(CubeCalculator.cube(3) === 27)
-    //assert(CubeCalculator.cube(0) === 23)
+    assert(CubeCalculator.cube(4) === 64)
   }
 }
